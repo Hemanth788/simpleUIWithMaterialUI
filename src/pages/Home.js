33a61662e55b2969cc2f6488c8c3aa0623fb38a5
Hemanth <hemanthsreenadhuni@gmail.com>
2022-03-26 15:54:00 +0530

    simple UI with MUI

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import "../App.css";
+import { Container, Grid, Typography } from "@mui/material";
+import TourCard from "../components/TourCard";
+import cities from "../data.json";
+function Home() {
+  return (
+    <div className="App">
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => {
+          return (
+            <>
+              <Typography
+                variant="h4"
+                component={"h2"}
+                marginTop={5}
+                marginBottom={3}
+              >
+                Top {city.name} Tours
+              </Typography>
+              <Grid container spacing={5}>
+                {city.tours.map((tour, index) => (
+                  <TourCard key={index} tour={tour} />
+                ))}
+              </Grid>
+            </>
+          );
+        })}
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
